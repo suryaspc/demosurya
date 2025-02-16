d306efd5a7f9d410a626b33e315ef454a31d3ba5
suryaspc <sc.chapagain@gmail.com>
2025-02-16 18:28:27 -0500

    Update HelloWorld.java

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -2,7 +2,7 @@ package basic;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {//from b11 branch
+	public static void main(String[] args) {
 		System.out.println("Hello There!");
 
 	}
